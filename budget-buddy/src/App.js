1baafde4a6e3bd450bf3aed5b06a6bd5ed846ff9
Kayton <kt.kaytonheng@gmail.com>
2021-11-11 12:32:20 +0800

    Added proper login page WITHOUT firebase function yet

--- budget-buddy/src/App.js
+++ budget-buddy/src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import "./App.css";
--- budget-buddy/src/pages/LoginPage.js
+++ budget-buddy/src/pages/LoginPage.js
@@ -1,9 +1,46 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import Button from "../components/Button";
+import firebase from "../firebase.js";
 
 function LoginPage() {
+  const auth = firebase.auth();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = () => {
+    console.log(email);
+    console.log(password);
+    console.log("Login called");
+  };
+
   return (
-    <div>
-      <h1>Hello world</h1>
+    <div className="LoginScreen">
+      <div className="container">
+        <h1>Login</h1>
+        <form className="add-form">
+          <div className="form-control">
+            <label>Email</label>
+            <input
+              type="text"
+              placeholder="Input email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="form-control">
+            <label>Password</label>
+            <input
+              type="text"
+              placeholder="Input password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+        </form>
+        <Button color="green" text="Login" onClick={login} />
+      </div>
     </div>
   );
 }
