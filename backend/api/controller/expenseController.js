1673a2be2b0c45819d8cb47192285b205206ea40
chwen <chiacy11@gmail.com>
2021-11-11 13:27:42 +0800

    getExpenseController

--- backend/api/controller/expenseController.js
+++ backend/api/controller/expenseController.js
@@ -1,8 +1,23 @@
+const Project = require("../model/projectModel");
+const Expense = require("../model/expenseModel");
+const { errorFormatter } = require("../utils/errorFormatter");
+const { OK } = require("http-status");
+
 const getExpenseController = async (req, res, next) => {
   const projectId = req.params.projectId;
 
   let projectExpenses;
   try {
-    // projectExpenses = await
-  } catch (error) {}
+    projectExpenses = await Project.find({ project_id: projectId });
+  } catch (error) {
+    return next(error);
+  }
+
+  if (projectExpenses.length <= 0) {
+    return res.status(OK).json({ message: "No expenses found" });
+  }
+
+  return res.status(OK).json({ result: projectExpenses });
 };
+
+module.exports = { getExpenseController };
