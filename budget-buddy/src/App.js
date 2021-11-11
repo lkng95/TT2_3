ca0e5ff731e55db0f6d3f55467b220bda88c8712
audrey-yjy <audreyyeo96@gmail.com>
2021-11-11 16:08:27 +0800

    Updated ProjectsPage
    - edited api function

--- budget-buddy/src/App.js
+++ budget-buddy/src/App.js
@@ -1,9 +1,13 @@
+import { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import ProjectsPage from "./pages/ProjectsPage";
+
 import "./App.css";
 
 function App() {
+  
+
   return (
     <BrowserRouter>
       <Routes>
