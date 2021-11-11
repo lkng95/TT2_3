cec0c045e9a57d83a32e3214066cba78523bfe92
chwen <chiacy11@gmail.com>
2021-11-11 12:22:01 +0800

    view and add expense route

--- backend/api/route/expenseRoute.js
+++ backend/api/route/expenseRoute.js
@@ -1,5 +1,15 @@
-const logger = require('pine/lib/logger');
+const logger = require("pine/lib/logger");
 const express = require("express");
-const projectControler = require('../controllers/expenseController')
+const {
+  getExpenseController,
+  addExpenseController,
+} = require("../controllers/expenseController");
+const { route } = require("../..");
 
 const router = express.Router();
+
+router.get("/:projectId/view-expenses", getExpenseController);
+
+router.post("/:projectId/add-expenses", addExpenseController);
+
+module.exports = router;
