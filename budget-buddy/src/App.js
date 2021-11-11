a4354511ef1328550b52f5d08169f56898b0473a
arifeman <arifeman407@gmail.com>
2021-11-11 14:43:23 +0800

    Moved Project to component folder, added sample data to test

--- budget-buddy/src/App.js
+++ budget-buddy/src/App.js
@@ -1,11 +1,18 @@
-import './App.css';
-import ProjectsPage from './components/ProjectsPage';
+import "./App.css";
+import ProjectsPage from "./components/ProjectsPage";
+import Project from "./components/Project";
 
 function App() {
   return (
     <div className="App">
       <ProjectsPage />
-      {/* <Project /> */}
+      <Project
+        id="1"
+        uid="4"
+        name="RTF"
+        budget="12000"
+        description="Realtime Face Recognition"
+      />
     </div>
   );
 }
