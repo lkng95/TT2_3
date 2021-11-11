8c4b3ef8c8223ddba9a802716bafbd3894a48e7f
tianhaowong <alamak213@msn.com>
2021-11-11 13:16:57 +0800

    keys

---
+++ backend/api/config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://dbsuser:dbs123@cluster0.u7kfp.mongodb.net/data?retryWrites=true&w=majority",
+};
+
+//mongodb account username: dbsseed23
+// mongodb password:        test123@@
