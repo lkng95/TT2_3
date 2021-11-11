32f70724ce85cb7d2c9550581c0e0ed123a65191
audrey-yjy <audreyyeo96@gmail.com>
2021-11-11 14:59:51 +0800

    Updated files
    - shifted ProjectsPage to /pages

--- budget-buddy/src/App.js
+++ budget-buddy/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
+import ProjectsPage from "./pages/ProjectsPage";
 import "./App.css";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<LoginPage />} />
+        <Route path="/projects" element={<ProjectsPage />} />
       </Routes>
     </BrowserRouter>
   );
